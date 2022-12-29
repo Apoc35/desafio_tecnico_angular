bf1858dc8a9425d39f5609d3dfebd508acd7e5fd
Apoc35 <alexv_silveira@outlook.com>
2022-12-28 23:17:55 -0300

    feat:  main page created and test adjustments

--- src/app/components/characterCard/character-card/character-card.component.spec.ts
+++ src/app/components/characterCard/character-card/character-card.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterCardComponent } from './character-card.component';
 
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+
 describe('CharacterCardComponent', () => {
   let component: CharacterCardComponent;
   let fixture: ComponentFixture<CharacterCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CharacterCardComponent ]
+      declarations: [ CharacterCardComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
 
