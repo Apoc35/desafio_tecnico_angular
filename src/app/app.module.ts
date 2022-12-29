9642de9459437a419af9d8897348e07c0c6d759b
Apoc35 <alexv_silveira@outlook.com>
2022-12-29 01:53:04 -0300

    fix: Fixing some import errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
+import { MainPageModule } from './components/main-page/main-page.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterCardComponent } from './shared/character-card/character-card.component';
 import { ContainerComponent } from './shared/container/container.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CharacterCardComponent,
     ContainerComponent,
-    MainPageComponent,
   ],
   imports: [
+    MainPageModule,
     HttpClientModule,
     FormsModule,
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/main-page/main-page.module.ts
+++ src/app/components/main-page/main-page.module.ts
@@ -3,10 +3,12 @@ import { MainPageComponent } from './main-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page.routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [MainPageModule],
+  declarations: [MainPageComponent],
   imports: [
+    RouterModule,
     CommonModule,
     MainPageRoutingModule
   ],
