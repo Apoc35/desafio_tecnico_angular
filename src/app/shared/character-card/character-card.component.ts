a738af585dd2a1b056e3decc2a6ccce5958efdc7
Apoc35 <alexv_silveira@outlook.com>
2022-12-29 23:52:41 -0300

    feat: configuring scss

--- src/app/shared/character-card/character-card.component.ts
+++ src/app/shared/character-card/character-card.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-character-card',
   templateUrl: './character-card.component.html',
-  styleUrls: ['./character-card.component.css']
+  styleUrls: ['./character-card.component.scss']
 })
 export class CharacterCardComponent {
 
