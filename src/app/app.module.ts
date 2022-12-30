ec72dfee128e8b069110d549e28be498ae0ee638
Apoc35 <alexv_silveira@outlook.com>
2022-12-30 02:44:59 -0300

    feat: home's css configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './../../shared/header/header.component';
 import { CharacterCardComponent } from './../../shared/character-card/character-card.component';
 import { ContainerComponent } from './../../shared/container/container.component';
 import { HomeComponent } from './home.component';
@@ -5,11 +6,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home.routing.module';
 
-
-
-
 @NgModule({
-  declarations: [HomeComponent, ContainerComponent, CharacterCardComponent],
+  declarations: [HomeComponent, ContainerComponent, CharacterCardComponent, HeaderComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
