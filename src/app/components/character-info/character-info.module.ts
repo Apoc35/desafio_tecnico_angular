8cd69d7715fb765798f34325672c4a66c378f3a7
Apoc35 <alexv_silveira@outlook.com>
2022-12-31 12:54:48 -0300

    fix: character info router fix

--- src/app/components/character-info/character-info.module.ts
+++ src/app/components/character-info/character-info.module.ts
@@ -1,7 +1,6 @@
 import { CharacterInfoRoutingModule } from './character-info.routing.module';
 import { ContainerComponent } from './../../shared/container/container.component';
 import { CharacterInfoComponent } from './character-info.component';
-import { HomeRoutingModule } from './../home/home.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -10,7 +9,6 @@ import { CommonModule } from '@angular/common';
   declarations: [CharacterInfoComponent, ContainerComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule,
     CharacterInfoRoutingModule
   ],
   exports: [
