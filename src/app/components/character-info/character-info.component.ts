2e62e1f221bf51daca95e97c7979e8f99323277c
Apoc35 <alexv_silveira@outlook.com>
2022-12-31 14:55:32 -0300

    fix: fixed character-info page

--- src/app/components/character-info/character-info.component.ts
+++ src/app/components/character-info/character-info.component.ts
@@ -1,3 +1,6 @@
+import { take } from 'rxjs';
+import { Character } from 'src/app/models/character.model';
+import { CharacterService } from 'src/app/services/character.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterInfoComponent implements OnInit {
 
-  constructor() { }
+  public $character = this.characterService.$characterData.asObservable();
+  public character: Character = {
+    id: 0,
+    name: '',
+    status: '',
+    species: '',
+    type: '',
+    gender: '',
+    origin: { name: '', url: '' },
+    location: { id: 0, name: '' },
+    episode: [],
+    image: '',
+    url: '',
+    created: ''
+  };
+
+  constructor(
+    private readonly characterService: CharacterService,
+  ) { }
 
   ngOnInit(): void {
+    this.$character.pipe(take(1)).subscribe((value) => {
+      this.character = value;
+    })
   }
 
 }
